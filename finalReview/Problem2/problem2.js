5651fce9acc48216b5f4e1a8c3f4001ede9a427e
Jay Schultz <jayjschultz@gmail.com>
2015-08-14 16:10:49 -0500

    hopefully last changes

--- finalReview/Problem2/problem2.js
+++ finalReview/Problem2/problem2.js
@@ -6,21 +6,22 @@ jQuery has been imported for you.
 A client has contacted you because they've written terrible JavaScript.
 Repair their code! There are 10 bugs in the code. Make a short description of what you've fixed.
 
-1.  ?
-2.  ?
-3.  ?
-4.  ?
-5.  ?
-6.  ?
-7.  ?
+1.  $
+2.  (
+3.  )
+4.  (
+5.  )
+6.  delete .
+7.  
 8.  ?
 9.  ?
 10. ?
 */
 
 $(document).ready(function () {
-    $('<nav/> <ul/> <li/>').css('list-style-type', square);
+    $('nav ul li').css('list-style-type', 'square');
 
-    $('.output').text('<p/>'.text().split().length());
+    $('.count').text($('p').text().split(" ").length);
 });
+
 
